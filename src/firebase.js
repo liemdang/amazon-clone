72a3389f964b0418e724976b0b86c8d46251c920
liem <liem.dang@hotmail.com>
2020-09-09 20:33:48 +0200

    inital setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDlonCYdox3C8AF78vJ2l_if-YuiGoynIc",
+  authDomain: "challenge-57725.firebaseapp.com",
+  databaseURL: "https://challenge-57725.firebaseio.com",
+  projectId: "challenge-57725",
+  storageBucket: "challenge-57725.appspot.com",
+  messagingSenderId: "694308275654",
+  appId: "1:694308275654:web:cdf3474b3aa1b372829ec7",
+  measurementId: "G-B92VYVEV65",
+};
